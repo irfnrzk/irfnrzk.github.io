6495cf6e43a75f57527a8bd75f03a866d8a9d710
irfan <irfan>
2017-04-30 16:49:43 +0800

    remove webresume v1

--- js/services/photos.js
+++
@@ -1,9 +0,0 @@
-app.factory('photos', ['$http', function($http) {
-  return $http.get('http://s3.amazonaws.com/codecademy-content/courses/ltp4/photos-api/photos.json')
-         .success(function(data) {
-           return data;
-         })
-         .error(function(err) {
-           return err;
-         });
-}]);
