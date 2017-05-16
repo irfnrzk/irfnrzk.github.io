2800192d2620f2694f6b2462143080091494aaca
irfnrzk <irfan.abrazak@gmail.com>
2017-05-16 08:15:36 +0800

    build 3

---
+++ js/hlight.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.arrow-next').mouseenter(function(){
+        $(this).fadeTo('slow',1);});
+
+
+        
+        
+    $('.arrow-next').mouseleave(function(){
+            $(this).fadeTo('fast',0.2);            
+        });
+    });
